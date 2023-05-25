fa35710bff105518b11da83d70d253f7f78cf6b8
rotex <dantejake48@gmail.com>
2023-05-26 00:48:23 +0100

    filter values in incoming log records using filter_datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """
 Logging Module
 """
+import logging
 import re
 from typing import List
 
@@ -17,3 +18,23 @@ def filter_datum(fields: List[str], redaction: str,
                 "{}={}{}".format(field, redaction, separator),
                 message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """init constructtor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum"""
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
