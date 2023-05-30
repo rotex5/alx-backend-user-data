992b370cea1e0890492352362ecedbdb88497ae5
rotex <dantejake48@gmail.com>
2023-05-30 06:31:08 +0100

    added function docstring

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,6 +11,7 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization header """
         if authorization_header is None:
             return None
         if type(authorization_header) is not str:
