814e76fc86dd9e48eb9f0bde03aa7ecfa9737b9e
rotex <dantejake48@gmail.com>
2023-05-30 05:58:52 +0100

    Request validation for BasicAuth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+API basic authentication.
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    class representation of an BasicAuth.
+    """
+    pass
