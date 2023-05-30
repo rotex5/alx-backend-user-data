6c069ebbf9d86696f6b0a8dfa2ad3061eb169d44
rotex <dantejake48@gmail.com>
2023-05-30 05:09:20 +0100

    adding doc strings

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,10 +11,14 @@ class Auth:
     class representation of an API authentication.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """True if path is not in excluded_paths, else False"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """return authorization header from a request object
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """returns currently authorized user"""
         return None
