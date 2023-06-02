610b75a7c97d1c4e1cb94decdd0262b84e76794a
rotex <dantejake48@gmail.com>
2023-06-02 03:39:18 +0100

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 API Session authentication.
 """
+import uuid
 from .auth import Auth
 
 
@@ -9,4 +10,16 @@ class SessionAuth(Auth):
     """
     class representation of an SessionAuth.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
