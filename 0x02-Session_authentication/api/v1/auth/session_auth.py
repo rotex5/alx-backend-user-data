18e4ee7c7f019ecc42ca49fcf63af0644d26d8cd
rotex <dantejake48@gmail.com>
2023-06-02 03:26:31 +0100

    preping empty sessionauth logic

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+API Session authentication.
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    class representation of an SessionAuth.
+    """
+    pass
