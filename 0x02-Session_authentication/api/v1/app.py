7063e92703987a02d00479ae72cac37fd1f584aa
rotex <dantejake48@gmail.com>
2023-06-02 04:02:26 +0100

    added another url path in list of excluded paths

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,12 +53,14 @@ def bf_request() -> str:
     if auth is None:
         pass
 
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    excluded_paths = ["/api/v1/status/",
+                      "/api/v1/unauthorized/",
+                      "/api/v1/forbidden/",
+                      "/api/v1/auth_session/login/"]
 
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None\
+                and auth.session_cookie(request) is None:
             abort(401)
 
         if auth.current_user(request) is None:
