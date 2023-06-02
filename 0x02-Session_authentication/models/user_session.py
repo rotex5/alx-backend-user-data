72079d9ef19455e6285bd41722e3ff5495ed19d6
rotex <dantejake48@gmail.com>
2023-06-02 06:00:20 +0100

    Sessions in database

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Implementation of User session module.
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """
+    User session representation.
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """Initializes a User session instance.
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
