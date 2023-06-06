f0597c97cea2d69c8cf53f758b64fda86d972725
rotex <dantejake48@gmail.com>
2023-06-06 20:05:36 +0100

    implemented hashing-salting function

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Authentication Module
+"""
+import bcrypt
+from typing import ByteString
+
+
+def _hash_password(password: str) -> ByteString:
+    """returns bytes(i.e Slat hashed password passed in)"""
+    encoded_pwd = password.encode()
+    hashed_pwd = bcrypt.hashpw(encoded_pwd, bcrypt.gensalt())
+    return hashed_pwd
