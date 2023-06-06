b8fb96f98c0bc489096d2770a3cf8f77da638a22
rotex <dantejake48@gmail.com>
2023-06-06 20:11:22 +0100

    change type hint

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,7 @@ import bcrypt
 from typing import ByteString
 
 
-def _hash_password(password: str) -> ByteString:
+def _hash_password(password: str) -> bytes:
     """returns bytes(i.e Slat hashed password passed in)"""
     encoded_pwd = password.encode()
     hashed_pwd = bcrypt.hashpw(encoded_pwd, bcrypt.gensalt())
