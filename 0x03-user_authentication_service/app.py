f6b9baf003de2bb902f621ec790aa645db43c1a5
rotex <dantejake48@gmail.com>
2023-06-07 09:59:40 +0100

    implemented User registeration endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,17 +2,31 @@
 """
 Route model for webApp
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/")
-def welcomeMessage():
+@app.route("/", methods=["GET"])
+def welcomeMessage() -> str:
     """Returns a welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_users():
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": "{}".format(user.email),
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
