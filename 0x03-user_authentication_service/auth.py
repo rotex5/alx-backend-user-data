666d8da73eb20f23b1edd1fd04e27017f11ef6eb
rotex <dantejake48@gmail.com>
2023-06-07 09:19:09 +0100

    added functionality for use registeration

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,9 @@
 """Authentication Module
 """
 import bcrypt
-from typing import ByteString
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +12,24 @@ def _hash_password(password: str) -> bytes:
     encoded_pwd = password.encode()
     hashed_pwd = bcrypt.hashpw(encoded_pwd, bcrypt.gensalt())
     return hashed_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register user if they do not already exist
+        """
+        try:
+            user_check = self._db.find_user_by(email=email)
+
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            user = self._db.add_user(email, str(hashed_pwd))
+            return user
+
+        raise ValueError("Use {} already exists".format(user_check.email))
