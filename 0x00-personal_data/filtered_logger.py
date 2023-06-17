6c2a9036130eaa2aa105b871834b990cf7b54c85
rotex <dantejake48@gmail.com>
2023-06-17 10:34:44 +0100

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,14 +3,8 @@
 Logging Module
 """
 import logging
-import mysql.connector
 import re
-from os import getenv
-from mysql.connector.connection import MySQLConnection
 from typing import List
-
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -44,55 +38,3 @@ class RedactingFormatter(logging.Formatter):
         message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
-
-
-def get_logger() -> logging.Logger:
-    """returns a logging.Logger object"""
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
-    stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    stream_handler.setFormatter(formatter)
-
-    logger.addHandler(stream_handler)
-    return logger
-
-
-def get_db() -> MySQLConnection:
-    """return connector object of a Mysql database"""
-    username = getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = getenv("PERSONAL_DATA_DB_NAME")
-    db_con = mysql.connector.connect(
-            user=username,
-            password=password,
-            host=host,
-            database=db_name
-            )
-    return db_con
-
-
-def main():
-    """
-    retrieve all rows in the users table and display
-    each row under a filtered format
-    """
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    field_names = [i[0] for i in cursor.description]
-
-    logger = get_logger()
-
-    for row in cursor:
-        message = "".join(f"{k}={str(v)}; " for k, v in zip(row, field_names))
-        logger.info(message.strip())
-
-    cursor.close()
-    db.close()
-
-    if __name__ == '__main__':
-        main()
